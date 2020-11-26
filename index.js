b24c8760e83d70089e3bca03cad92af0c34e0550
rumikoacopa <rumikorios@gmail.com>
2020-11-26 03:06:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Upper West Side';
+mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Lauren Hart';
+companyCeo = 'Susan Smith';
+
+
+
+
+
